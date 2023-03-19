a184a548c454bb8ebf44e3c7c7236de6602e9f7c
Victor78970513 <yer29.chok@gmail.com>
2023-03-19 19:47:36 -0400

    Backend Comunication Finished

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,11 +1,31 @@
+const { checkJWT } = require('../helpers/jwt');
 const { io } = require('../index');
-
+const {usuarioConectado,usuarioDesconectado, grabarMensaje} = require('../controllers/socket')
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
+    const [valido,uid] = checkJWT(client.handshake.headers['x-token'])
+    
+    //verificar autenticacion
+    if(!valido){return client.disconnect();}
+
+    // Cliente autenticado
+    usuarioConectado(uid);
+
+    // Ingresar al usaurio a una sala en particular
+    // sala global, client
+    client.join( uid );
+
+    // Escuchar del cliente el mensaje personal
+    client.on('mensaje-personal', async (payload) =>{
+        //TODO GRABAR MENSAJE
+        await grabarMensaje(payload);
+        io.to(payload.para).emit('mensaje-personal',payload);
+    });
+
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
     // client.on('mensaje', ( payload ) => {
     //     console.log('Mensaje', payload);
